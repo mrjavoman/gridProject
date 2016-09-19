f6bbc152fa1da1255095cac00ead9646b183d2bc
Xavi <mrjavoman@hotmail.com>
2016-09-18 19:42:45 -0500

    initial commit

---
+++ src/components.js
@@ -0,0 +1,56 @@
+import React from 'react';
+//import { MyFirstGrid } from './reactgrid';
+
+var ReactGridLayout = require('react-grid-layout');
+
+export function Todo(props) {
+  const { todo } = props;
+
+  if(todo.isDone) {
+    return <strike>{todo.text}</strike>;
+  } else {
+    return <span>{todo.text}</span>;
+  }
+}
+
+
+export function Grid(props) {
+    
+    // layout is an array of objects, see the demo for more complete usage
+    var layout = [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ];
+    return (
+      <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key={'a'}>a</div>
+        <div key={'b'}>b</div>
+        <div key={'c'}>c</div>
+      </ReactGridLayout>
+    )
+}
+
+export function TodoList(props) {
+  const { todos, toggleTodo, addTodo } = props;
+
+  const onSubmit = (event) => {
+    const input = event.target;
+    const text = input.value;
+    const isEnterKey = (event.which == 13);
+    const isLongEnough = text.length > 0;
+
+    if(isEnterKey && isLongEnough) {
+      input.value = '';
+      addTodo(text);
+    }
+  };
+
+  const toggleClick = id => event => toggleTodo(id);
+
+  return (
+    <div className='todo'>
+      <Grid style="background-color:gray" />
+    </div>
+  );
+}
